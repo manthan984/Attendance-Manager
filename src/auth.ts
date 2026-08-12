de9d76aab2f547952bb08082c4879a346847e063
Manthan984 <177267908+manthan984@users.noreply.github.com>
2026-08-12 17:08:30 +0530

    Allow OAuth email linking

--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,10 @@ import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google, GitHub],
+  providers: [
+    Google({ allowDangerousEmailAccountLinking: true }),
+    GitHub({ allowDangerousEmailAccountLinking: true }),
+  ],
   pages: {
     signIn: "/login",
   },
